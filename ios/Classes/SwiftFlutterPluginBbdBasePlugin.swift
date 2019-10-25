7f7ac141554220b4b397d39fcd560bcf66bc438f
Oliver Zernikow <zernikowoliver@gmail.com>
2019-10-25 20:32:18 +0200

    rm unused code

--- ios/Classes/SwiftFlutterPluginBbdBasePlugin.swift
+++ ios/Classes/SwiftFlutterPluginBbdBasePlugin.swift
@@ -11,14 +11,6 @@ import GD.Runtime
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     switch call.method {
-    case "getPlatformVersion":
-        result("iOS " + UIDevice.current.systemVersion)
-        break;
-    case "getUserId":
-        let data = GDiOS.sharedInstance().getApplicationConfig();
-        let userId = data.first(where: {$0.key == "userId"})?.value;
-        result(userId);
-        break;
     default:
         result("Method not found");
         break;
